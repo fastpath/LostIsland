199c5bbc90de6ce11672661883096e1f9f5692a8
sascha <skolodze@uos.de>
2012-05-25 17:32:12 +0200

    tr1 defines

--- LostIsland/stdafx.h
+++ LostIsland/stdafx.h
@@ -29,6 +29,8 @@
 #include <d3d11.h>
 #include <D3DX11.h>
 #include <xnamath.h>
+#include <memory>
+
 #pragma comment(lib, "d3d11.lib")
 #pragma comment(lib, "d3dx11.lib")
 
@@ -41,3 +43,5 @@ extern BOOL g_bContinue;
 #define SAFE_RELEASE(_resource) { if((_resource) != NULL) { (_resource)->Release(); (_resource) = NULL; } }
 #define SAFE_DELETE(_ptr) { if((_ptr) != NULL) { delete (_ptr); (_ptr) = NULL; } }
 #define RETURN_IF_FAILED(_hr, _errorMsg) { if(FAILED(_hr)) return hr; } // TODO: output message
+#define shared_ptr std::tr1::shared_ptr;
+#define weak_ptr std::tr1::weak_ptr;
